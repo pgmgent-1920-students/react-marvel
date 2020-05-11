d3e6b998ed0ef9145f8184534ed4aef1db8931c0
drdynscript <drdynscript@gmail.com>
2020-05-11 13:36:27 +0200

    Deploy to docs

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -14,7 +14,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">      
-      <Router>
+      <Router basename="react-marvel">
         <Header />
         <Switch>
           <Route exact path="/comics">
---
+++ docs/precache-manifest.56deb1745f55f1765d3903bee1e471eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edeb5885afc00a3195101752f45206f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d82adae5b81fd8eaccea",
+    "url": "/static/css/main.f2cc6548.chunk.css"
+  },
+  {
+    "revision": "cb3acd26e1e66f506edb",
+    "url": "/static/js/2.172171e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.172171e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82adae5b81fd8eaccea",
+    "url": "/static/js/main.091fea52.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
